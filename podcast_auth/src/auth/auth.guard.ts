64f58539bcdaac7f80013dd0a327a7ff31a31671
jungmiko <jungmiko@jungmikoui-Air.attlocal.net>
2021-01-19 20:27:24 -0800

    first commit

---
+++ podcast_auth/src/auth/auth.guard.ts
@@ -0,0 +1,33 @@
+import { User } from './../users/entities/user.entity';
+import { AllowedRoles } from './role.decorator';
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { GqlExecutionContext } from '@nestjs/graphql';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+
+  constructor(
+    private readonly reflector: Reflector){}
+
+  canActivate(context: ExecutionContext) {
+    const roles = this.reflector.get<AllowedRoles>(
+      'roles',
+       context.getHandler(),
+    )
+
+    if(!roles){
+      return true;
+    }
+    const gqlContext = GqlExecutionContext.create(context).getContext();
+    const user:User= gqlContext['user'];
+    if (!user) {
+      return false;
+    }
+
+    if(roles.includes("Any")){
+      return true;
+    }
+    return roles.includes(user.role)
+  }
+}
